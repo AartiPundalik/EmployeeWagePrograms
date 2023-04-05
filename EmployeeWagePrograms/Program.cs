9e63b10bc58cb9d961202d02fdf0e0963a0dbc22
AartiPundalik <aartipundalik68@gmail.com>
2023-04-05 17:07:29 +0530

    [Aarti] Add : UC1 - Checking Attendence Of Employee

---
+++ EmployeeWagePrograms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWagePrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckAttendence checkattendence = new CheckAttendence();
+            checkattendence.Attendence();
+            Console.ReadLine();
+
+        }
+    }
+}
